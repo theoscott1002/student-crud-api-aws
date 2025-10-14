89847eaab5c12614f7294e265edec93dcdf6fe9a
theoscott1002 <ariyotheophilus@gmail.com>
2025-10-14 02:23:35 +0100

    code lint5

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.exc import SQLAlchemyError
 
-# Define db and migrate globally
+# Define db & migrate globally
 db = SQLAlchemy()
 migrate = Migrate()
 
